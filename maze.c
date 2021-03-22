9ac66b2b51cbc54efb8f8725cd82357d41893a94
deblur99 <deblur99@kakao.com>
2021-03-22 13:05:51 +0900

    origin-master

--- maze.c
+++ maze.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
+
+#define BUFFER_LENGTH 2
+#define BUFFER_SIZE_LENGTH 20
+#define BUFFER_AMOUNT_MAZES_LEGNTH 5
+#define DIRECTION_BUFFER_LENGTH 10
 
 typedef struct size {
     int row;
     int col;
-
+}SIZE;
 
 // find actual amount of mazes in the file
 int findAmountMaze(FILE *fp) {
 
     int amount_mazes;
     char *buffer = (char *)malloc(sizeof(char));
-    char buffer_amount_mazes[5];
+    char buffer_amount_mazes[BUFFER_AMOUNT_MAZES_LEGNTH];
 
     fread(buffer, sizeof(char), 1, fp); 
     strcpy(buffer_amount_mazes, buffer);
@@ -27,150 +31,177 @@ int findAmountMaze(FILE *fp) {
 
     fgetc(fp); // move as 1
     free(buffer);
-    buffer = NULL;
 
     return amount_mazes;
 }
 
-// find rows and columns of a maze
-SIZE getSizeOfMaze(FILE *fp) {
+// main problem: find directions of maze
+int findDirection(int row_position, int col_position, SIZE size, int *(arr)[size.col], int pre_row_compare) {
 
-    SIZE size;
-    char *buffer = (char *)malloc(sizeof(char));
-    char *buffer_size = (char *)malloc(sizeof(char) * 20);
-    char *tok, *temp;
+    // Idea:
+        // 위->아래->위 (0, 0), 0 -> (1, 0), 0 -> (0, 0), 1
+        // 아래->위->아래 (1, 1), 2 -> (0, 1), 1 -> (1, 1), 0
+        // 이전 단계의 row만 볼 수 있음 -> 핵심은 현 단계 row와 이전의 이전 단계 row의 비교
 
-    while (strcmp(buffer, "\r") != 0) {
-        fread(buffer, sizeof(char), 1, fp);
-        strcat(buffer_size, buffer);
-    }
-    
-    // find integer values and put these into each members of struct size
-    tok = strtok(buffer_size, " ");
-    temp = tok;
-    size.row = atoi(temp);
+        // 이미 지나간 경로로 가지 않기 위한 규칙
+        // 이전에 위에서 내려온 경우: 위로 다시 갈 수 없으므로 오른쪽 또는 아래쪽으로 진행 가능
+        // 이전에 아래에서 올라온 경우: 아래로 다시 갈 수 없으므로 위쪽 또는 오른쪽으로 진행 가능
+        // 이전에 왼쪽에서 오른쪽으로 온 경우: 왼쪽으로 가는 동작은 없으므로 위쪽 또는 오른쪽 또는 아래쪽으로 진행 가능
 
-    tok = strtok(NULL, " ");
-    temp = tok;
-    size.col = atoi(tok);
+    if (row_position < 0 || row_position == size.row || col_position == size.col)
+        return 0;
 
-    // exception: if there are more than two values that implying row and column line,
-    //            all members in structure will have 0 and be returned.
-    tok = strtok(NULL, " ");
-    if (tok != NULL) {
-        printf("Error: Invalid Input size\n");
-        size.row = 0; size.col = 0;
-        return size;
+    if (arr[row_position][col_position] == 0)
+        return 0;
+
+    if ((row_position == size.row - 1) && (col_position == size.col - 1)) {
+        return 1;
     }
 
-    fgetc(fp); // move from "\n" to first value
-    free(buffer_size);
-    free(buffer);
-
-    buffer_size = NULL;
-    buffer = NULL;
-
-    return size;
+    if (pre_row_compare - 1 == row_position)
+        return (findDirection(row_position - 1, col_position, size, arr, row_position) +
+                findDirection(row_position, col_position + 1, size, arr, row_position));
+    else if (pre_row_compare + 1 == row_position)
+        return (findDirection(row_position, col_position + 1, size, arr, row_position) +
+                findDirection(row_position + 1, col_position, size, arr, row_position));
+    else
+        // each function call means going up, going right, going down
+        return (findDirection(row_position - 1, col_position, size, arr, row_position) +
+                findDirection(row_position, col_position + 1, size, arr, row_position) +
+                findDirection(row_position + 1, col_position, size, arr, row_position)); 
 }
-
-// get a maze from the file
-int** getMaze(FILE *fp, SIZE size) {
-
-    // allocate dynamic memory
-    char *buffer = (char *)malloc(sizeof(char) * 2);
-    int **arr = (int **)malloc(sizeof(int *) * size.row);
-    
-    for (int i = 0; i < size.row; i++) {
-        arr[i] = (int *)malloc(sizeof(int) * size.col);
-    }
-
-    // get a maze with conditions
-    for (int i = 0; i < size.row; i++) {
-        for (int j = 0; j < size.col; j++) {
-            fread(buffer, sizeof(char), 2, fp);
-
-            if (strcmp(buffer, " ") == 0) {
-                j--;
-                continue;
-            }
-
-            // save value into array
-            arr[i][j] = atoi(buffer);
-        }
-
-        // make fp to go down a line
-        fgetc(fp);
-        fgetc(fp);
-    }
-
-    free(buffer);
-    buffer = NULL;
-
-    // return the gotten maze
-    return arr;
-}
-
-// actual main function
-int findDirections(int *maze[], int row, int col);
 
 int main() {
+    // create file pointers
     FILE *fp = fopen("data_structure_and_algorithm/input_ex.txt", "r");
-    FILE *fp_out = fopen("data_structure_and_algorithm/output_ex.txt", "w");
+    FILE *fp_out = fopen("data_structure_and_algorithm/output.txt", "w");
 
-    // initialization
+    // initialization variables
     int amount_mazes = 0, directions = 0;
+    int result = 0;
+    char *buffer = NULL;
+    char *buffer_size = NULL;
+    char *tok;
+    char temp[DIRECTION_BUFFER_LENGTH] = {'\0', };
+
+    SIZE size = {0, 0};
+    SIZE position = {0, 0};
     
     // get amount of all mazes
     if (ftell(fp) == 0)
         amount_mazes = findAmountMaze(fp);    
 
+    // if amount_mazes is 0, then quit program
     if (amount_mazes == 0) {
         fclose(fp_out);
         fclose(fp);
         return 0;
-    }      
-    
+    }
+
     // get size of maze
     while (amount_mazes > 0) {
+        if (buffer == NULL)
+            buffer = (char *)malloc(sizeof(char) * BUFFER_LENGTH);
+
         amount_mazes--;
+        size.row = 0; size.col = 0;
 
-        SIZE size_of_maze = getSizeOfMaze(fp);
+        // find rows and columns of a maze
+        // and get integer values and put these into each members of struct size
+        while (strcmp(buffer, "\r") != 0) {
+            if (size.row == 0 || size.col == 0) {
+                buffer_size = (char *)malloc(sizeof(char) * BUFFER_SIZE_LENGTH);
+                memset(buffer_size, 0, sizeof(char) * BUFFER_SIZE_LENGTH);
+                memset(buffer, 0, sizeof(char) * BUFFER_LENGTH);
+            }
 
-        // get a maze
-        int **maze = getMaze(fp, size_of_maze);
+            fread(buffer, sizeof(char), 1, fp);
+            strcpy(buffer_size, buffer);
 
-        // find directions of the maze
-        directions = findDirections(maze, size_of_maze.row, size_of_maze.col);
-
-        // debug code
-        printf("%d %d\n", size_of_maze.row, size_of_maze.col);
-        for (int i = 0; i < size_of_maze.row; i++) {
-            for (int j = 0; j < size_of_maze.col; j++)
-                printf("%d ", maze[i][j]);
-            printf("\n");
-        }    
-
-        // free dynamic memories and close file pointers
-        for (int i = 0; i < size_of_maze.row; i++) {
-            free(maze[i]);
+            if (size.row == 0) {
+                while (strcmp(buffer, " ")) {
+                    fread(buffer, sizeof(char), 1, fp);
+                    strcat(buffer_size, buffer);
+                }
+                size.row = atoi(buffer_size);
+            }
+            else if (size.col == 0) {
+                while (strcmp(buffer, "\r")) {
+                    fread(buffer, sizeof(char), 1, fp);
+                    strcat(buffer_size, buffer);
+                }
+                size.col = atoi(buffer_size);
+            }
         }
 
-        free(maze);
-        maze = NULL;
+        fgetc(fp);
 
-        printf("%ld\n", ftell(fp));
-        char buffer[1];
-        fread(buffer, sizeof(char), 1, fp);
-        printf("%d\n", atoi);
-        printf("%ld\n", ftell(fp));
+        free(buffer_size);
+        free(buffer);
+
+        buffer_size = NULL;
+        buffer = NULL;
+
+        // allocate dynamic memory of maze
+        buffer = (char *)malloc(sizeof(char) * BUFFER_LENGTH);
+        int **arr = (int **)malloc(sizeof(int *) * size.row);
+
+        for (int i = 0; i < size.row; i++) {
+            arr[i] = (int *)malloc(sizeof(int) * size.col);
+        }
+
+        // get a maze with conditions
+        for (int i = 0; i < size.row; i++) {
+            for (int j = 0; j < size.col; j++) {
+                fread(buffer, sizeof(char), 2, fp);
+
+                if (strcmp(buffer, " ") == 0) {
+                    j--;
+                    continue;
+                }
+
+                // save value into array
+                arr[i][j] = atoi(buffer);
+            }
+
+            // make fp to go down a line
+            fgetc(fp);
+            fgetc(fp);
+        }        
+
+        for (int i = 0; i < size.row; i++) {
+            for (int j = 0; j < size.col; j++)
+                printf("%d ", arr[i][j]);
+            printf("\n");
+        }
+
+        // find a number of directions of the maze
+        position.row = 0; position.col = 0;
+        result = findDirection(position.row, position.col, size, arr, 0);
+
+        // write a number of directions of the maze into output.txt
+        sprintf(temp, "%d\n", result);
+        fwrite(temp, sizeof(char), strlen(temp), fp_out);
+
+        memset(temp, 0, DIRECTION_BUFFER_LENGTH);
+
+        // free the maze array
+        for (int i = 0; i < size.row; i++) {
+            free(arr[i]);
+        }
+        
+        free(arr);
+        free(buffer);
+
+        arr = NULL;
+        buffer = NULL;
     }
+
+    free(buffer);
+    buffer = NULL;
 
     fclose(fp_out);
     fclose(fp);
 
-    return 0;
-}
-
-int findDirections(int *maze[], int row, int col) {
     return 0;
 }
